2539cd02c60247e5867dee57e40a41c371692bab
Julius <juliussowah@amalitech.com>
2025-03-31 23:39:24 +0000

    feat: Implmented the exception classes to hadnle error's in the application

---
+++ src/main/java/com/giftedlabs/tasksphere/exception/ErrorResponse.java
@@ -0,0 +1,23 @@
+package com.giftedlabs.tasksphere.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private int status;
+    private String message;
+    private String path;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
+
+}
