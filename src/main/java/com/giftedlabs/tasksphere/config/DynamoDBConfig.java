f8612806d58312e140fcb45e0d33ae2c202ca4de
Julius <juliussowah@amalitech.com>
2025-04-01 10:19:01 +0000

    feat: Debugged the application to fix the authentication issues

--- src/main/java/com/giftedlabs/tasksphere/config/DynamoDBConfig.java
+++ src/main/java/com/giftedlabs/tasksphere/config/DynamoDBConfig.java
@@ -1,24 +1,34 @@
 package com.giftedlabs.tasksphere.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.*;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 
+@Slf4j
 @Configuration
 public class DynamoDBConfig {
 
-    @Value("${aws.region}")
-    private String region;
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
 
     @Bean
     public DynamoDbClient dynamoDbClient(){
+
+        AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(accessKey,secretKey);
+        StaticCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(awsCredentials);
+
         return DynamoDbClient.builder()
-                .region(Region.of(region))
-                .credentialsProvider(DefaultCredentialsProvider.create())
+                .region(Region.EU_CENTRAL_1)
+                .credentialsProvider(credentialsProvider)
                 .build();
     }
 
